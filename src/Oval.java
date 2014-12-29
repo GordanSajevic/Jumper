3eefccb4451fcb4dc09c40450c2d914d050f813b
Gordan Sajević <gsajevic@hotmail.com>
2014-12-29 22:57:52 +0100

    Update Oval.java

--- src/Oval.java
+++ src/Oval.java
@@ -4,10 +4,30 @@ import java.awt.Graphics;
 
 public class Oval extends Body{
 
+	/**
+	 * Konstruktor koji prima sve parametre
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 * @param color
+	 * @param speedX
+	 * @param speedY
+	 * @param maxX
+	 * @param maxY
+	 * @param minX
+	 * @param minY
+	 */
+	
 	public Oval(int x, int y, int width, int height, Color color, int speedX, int speedY, int maxX, int maxY, int minX, int minY) {
 		super(x, y, width, height, color, speedX, speedY, maxX,  maxY, minX, minY);
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Override metode draw. Kako je u ovoj klasi jasno koju figuru trebamo crtati, pozivamo fillOval
+	 *  i šaljemo odgovarajuće parametre
+	 */
 	
 	@Override
 	public void draw(Graphics g)
